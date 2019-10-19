51f810b331c00b424d7e74e80e52cce86f33e81b
Anu Nairis <anu.nairis90@gmail.com>
2019-10-19 16:39:25 +0300

    finised project

--- CodeReview_6/basicEx.js
+++ CodeReview_6/basicEx.js
@@ -12,11 +12,13 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 var Location1 = /** @class */ (function () {
-    function Location1(image, place, address, city, zip) {
+    function Location1(placeTitle, image, place, address, city, zip) {
+        this.placeTitle = "";
         this.image = "";
         this.place = "";
         this.address = "";
         this.city = "";
+        this.placeTitle = placeTitle;
         this.image = image;
         this.place = place;
         this.address = address;
@@ -24,64 +26,53 @@ var Location1 = /** @class */ (function () {
         this.zip = zip;
     }
     Location1.prototype.display = function () {
-        var showDisplay = "\n  <div class=\"row col-lg-3 col-md-6 col-sm-12 mt-2 offset\" id=\"places\">\n  <div class=\"card\">\n    <div class=\"card-body py-0 px-0\">\n      <img src=\"" + this.image + "\" alt=\"\" height=\"250px\" max-width=\"100%\" class=\"card-img-top col-lg-12 img-responsive\">\n      <h5 class=\"card-title ml-3\">" + this.place + "</h5>\n      <ul>\n      <li class=\"card-text\">" + this.address + ", " + this.zip + " " + this.city + "</li>\n      </ul>\n      </div>\n    </div>\n  </div>";
+        console.log(this.placeTitle);
+        var showDisplay = "\n    \n    <div class=\"row col-lg-3 col-md-6 col-sm-12 mt-2 offset\" id=\"places\">\n    <h4 class=\"ml-3 mt-3 mr-3 title-section\"><strong>" + this.placeTitle + "</strong></h4>\n    <div class=\"card\">\n      <div class=\"card-body py-0 px-0 body-section\">\n        <img src=\"" + this.image + "\" alt=\"\" height=\"250px\" max-width=\"100%\" class=\"card-img-top col-lg-12 img-responsive lose-picture\">\n        <h5 class=\"card-title ml-3\">" + this.place + "</h5>\n        <ul>\n        <li class=\"card-text\">" + this.address + ", " + this.zip + " " + this.city + "</li>\n        </ul>\n        </div>\n      </div>\n    </div>\n  </div>\n";
         return showDisplay;
     };
     return Location1;
 }());
 var Restaurant = /** @class */ (function (_super) {
     __extends(Restaurant, _super);
-    function Restaurant(image, place, address, city, zip, phone, webpage, foodServed) {
-        var _this = _super.call(this, image, place, address, city, zip) || this;
+    function Restaurant(placeTitle, image, place, address, city, zip, phone, webpage, foodServed) {
+        var _this = _super.call(this, placeTitle, image, place, address, city, zip) || this;
         _this.phone = phone;
         _this.webpage = webpage;
         _this.foodServed = foodServed;
         return _this;
     }
-    Restaurant.prototype.displayRestaurant = function () {
-        var showDisplayRestaurant = "\n  <div class=\"row col-lg-3 col-md-6 col-sm-12 mt-2 offset\" id=\"restaurants\">\n  <div class=\"card\">\n    <div class=\"card-body py-0 px-0\">\n      <img src=\"" + this.image + "\" alt=\"\" height=\"250px\" max-width=\"100%\" class=\"card-img-top col-lg-12 img-responsive\">\n      <h5 class=\"card-title ml-3\">" + this.place + "</h5>\n      <ul>\n      <li class=\"card-text\">" + this.address + ", " + this.zip + " " + this.city + "</li>\n      <li class=\"card-text\">" + this.phone + "</li>\n      <li><a href=\"" + this.webpage + "\">" + this.webpage + "</a></li>\n      <li class=\"card-text\">" + this.foodServed + "</li>\n      </ul>\n      </div>\n    </div>\n  </div>";
+    Restaurant.prototype.display = function () {
+        _super.prototype.display.call(this);
+        var showDisplayRestaurant = "\n  \n  <div class=\"row col-lg-3 col-md-6 col-sm-12 mt-2 offset\" id=\"restaurants\">\n  <h4 class=\"ml-3 mt-3 mr-3 title-section\"><strong>" + this.placeTitle + "</strong></h4>\n  <div class=\"card\">\n    <div class=\"card-body py-0 px-0 body-section\">\n      <img src=\"" + this.image + "\" alt=\"\" height=\"250px\" max-width=\"100%\" class=\"card-img-top col-lg-12 img-responsive lose-picture\">\n      <h5 class=\"card-title ml-3\">" + this.place + "</h5>\n      <ul>\n      <li class=\"card-text\">" + this.address + ", " + this.zip + " " + this.city + "</li>\n      <li class=\"card-text\">" + this.phone + "</li>\n      <li><a href=\"" + this.webpage + "\">" + this.webpage + "</a></li>\n      <li class=\"card-text\">" + this.foodServed + "</li>\n      </ul>\n      </div>\n    </div>\n  </div>";
         return showDisplayRestaurant;
     };
     return Restaurant;
 }(Location1));
 var Events = /** @class */ (function (_super) {
     __extends(Events, _super);
-    function Events(image, place, address, city, zip, eventDate, eventTime, price, webpage) {
-        var _this = _super.call(this, image, place, address, city, zip) || this;
+    function Events(placeTitle, image, place, address, city, zip, eventDate, eventTime, price, webpage) {
+        var _this = _super.call(this, placeTitle, image, place, address, city, zip) || this;
         _this.eventDate = eventDate;
         _this.eventTime = eventTime;
         _this.price = price;
         _this.webpage = webpage;
         return _this;
     }
-    Events.prototype.displayEvents = function () {
-        var showDisplayEvents = "\n  <div class=\"row col-lg-3 col-md-6 col-sm-12 mt-2 offset\" id=\"events\">\n  <div class=\"card\">\n    <div class=\"card-body py-0 px-0\">\n      <img src=\"" + this.image + "\" alt=\"\" height=\"250px\" max-width=\"100%\" class=\"card-img-top col-lg-12 img-responsive\">\n      <h5 class=\"card-title ml-3\">" + this.place + "</h5>\n      <ul>\n      <li class=\"card-text\">" + this.address + ", " + this.zip + " " + this.city + "</li>\n      <li class=\"card-text\">" + this.eventDate + "</li>\n      <li class=\"card-text\">" + this.eventTime + "</li>\n      <li class=\"card-text\">Ticket price: " + this.price + " EUR</li>\n      <li><a href=\"" + this.webpage + "\">" + this.webpage + "</a></li>\n      </ul>\n      </div>\n    </div>\n  </div>";
+    Events.prototype.display = function () {
+        _super.prototype.display.call(this);
+        var showDisplayEvents = "\n \n  <div class=\"row col-lg-3 col-md-6 col-sm-12 mt-2 offset\" id=\"events\">\n   <h4 class=\"ml-3 mt-3 mr-3 title-section\"><strong>" + this.placeTitle + "</strong></h4>\n  <div class=\"card\">\n    <div class=\"card-body py-0 px-0 body-section\">\n      <img src=\"" + this.image + "\" alt=\"\" height=\"250px\" max-width=\"100%\" class=\"card-img-top col-lg-12 img-responsive lose-picture\">\n      <h5 class=\"card-title ml-4\">" + this.place + "</h5>\n      <ul>\n      <li class=\"card-text\">" + this.address + ", " + this.zip + " " + this.city + "</li>\n      <li class=\"card-text\">" + this.eventDate + "</li>\n      <li class=\"card-text\">" + this.eventTime + "</li>\n      <li class=\"card-text\">Ticket price: " + this.price + " EUR</li>\n      <li><a href=\"" + this.webpage + "\">" + this.webpage + "</a></li>\n      </ul>\n      </div>\n    </div>\n  </div>";
         return showDisplayEvents;
     };
     return Events;
 }(Location1));
-var karlsplatz = new Location1("img/karls.jpg", "St. Charles Church", "Karlsplatz 1", "Vienna", 1010);
-var zoo = new Location1("img/zoo.jpg", "Zoo Vienna", "Maxingstraße 13b", "Vienna", 1130);
-var lemonResto = new Restaurant("img/lemon.png", "Lemon Leaf Thai Restaurant", "Kettenbrückengasse 19", "Vienna", 1050, "+43(1)5812308", "http://www.lemonleaf.at/", "Chinese Restaurant");
-var sixtaResto = new Restaurant("img/sixta.png", "Sixta Restaurant Bar", "Schönbrunner Straße 21", "Vienna", 1050, "+43 1 58 528 56", "http://www.sixta-restaurant.at/", "Viennese Restaurant");
-var krisConcert = new Events("img/kris.jpg", "Kris Kristofferson Concert", "Wiener Stadthalle(Halle E), Roland Rainer Platz 1", "Vienna", 1150, "Fr., 15.11.2019", "20:00", "58.50", "http://kriskristofferson.com/");
-var lennyConcert = new Events("img/lenny.jpg", "Lenny Kravitz Concert", "Wiener Stadthalle(Halle D), Roland Rainer Platz 1", "Vienna", 1150, "Sat, 09.12.2019", "19:30", "47.80", "http://www.lennykravitz.com/");
+var karlsplatz = new Location1("PLACES:", "img/karls.jpg", "St. Charles Church", "Karlsplatz 1", "Vienna", 1010);
+var zoo = new Location1("", "img/zoo.jpg", "Zoo Vienna", "Maxingstraße 13b", "Vienna", 1130);
+var lemonResto = new Restaurant("RESTAURANTS:", "img/lemon.jpg", "Lemon Leaf Thai Restaurant", "Kettenbrückengasse 19", "Vienna", 1050, "+43(1)5812308", "http://www.lemonleaf.at", "Chinese Restaurant");
+var sixtaResto = new Restaurant("", "img/sixta.png", "Sixta Restaurant Bar", "Schönbrunner Straße 21", "Vienna", 1050, "+43 1 58 528 56", "http://www.sixta-restaurant.at", "Viennese Restaurant");
+var krisConcert = new Events("EVENTS:", "img/kris.jpg", "Kris Kristofferson Concert", "Wiener Stadthalle(Halle E), Roland Rainer Platz 1", "Vienna", 1150, "Fr., 15.11.2019", "20:00", "58.50", "http://kriskristofferson.com");
+var lennyConcert = new Events("", "img/lenny.jpg", "Lenny Kravitz Concert", "Wiener Stadthalle(Halle D), Roland Rainer Platz 1", "Vienna", 1150, "Sat, 09.12.2019", "19:30", "47.80", "http://www.lennykravitz.com");
 var array = [karlsplatz, zoo, lemonResto, sixtaResto, krisConcert, lennyConcert];
 var cards = document.getElementById("result");
 for (var i in array) {
-    if (i == "4") {
-        cards.innerHTML += array[i].displayEvents();
-    }
-    else if (i == "5") {
-        cards.innerHTML += array[i].displayEvents();
-    }
-    else if (i == "2") {
-        cards.innerHTML += array[i].displayRestaurant();
-    }
-    else if (i == "3") {
-        cards.innerHTML += array[i].displayRestaurant();
-    }
-    else {
-        cards.innerHTML += array[i].display();
-    }
+    cards.innerHTML += array[i].display();
 }
--- CodeReview_6/basicEx.ts
+++ CodeReview_6/basicEx.ts
@@ -1,10 +1,12 @@
 class Location1 {
+  placeTitle = "";
   image = "";
   place = "";
   address = "";
   city = "";
   zip:number; 
-constructor(image:string, place:string, address:string, city:string, zip) { 
+constructor(placeTitle:string, image:string, place:string, address:string, city:string, zip) { 
+  this.placeTitle = placeTitle;
   this.image = image;
   this.place = place;
   this.address = address;
@@ -13,39 +15,46 @@ constructor(image:string, place:string, address:string, city:string, zip) {
   }
 
 display() {
+  console.log(this.placeTitle);
   let showDisplay = `
-  <div class="row col-lg-3 col-md-6 col-sm-12 mt-2 offset" id="places">
-  <div class="card">
-    <div class="card-body py-0 px-0">
-      <img src="${this.image}" alt="" height="250px" max-width="100%" class="card-img-top col-lg-12 img-responsive">
-      <h5 class="card-title ml-3">${this.place}</h5>
-      <ul>
-      <li class="card-text">${this.address}, ${this.zip} ${this.city}</li>
-      </ul>
+    
+    <div class="row col-lg-3 col-md-6 col-sm-12 mt-2 offset" id="places">
+    <h4 class="ml-3 mt-3 mr-3 title-section"><strong>${this.placeTitle}</strong></h4>
+    <div class="card">
+      <div class="card-body py-0 px-0 body-section">
+        <img src="${this.image}" alt="" height="250px" max-width="100%" class="card-img-top col-lg-12 img-responsive lose-picture">
+        <h5 class="card-title ml-3">${this.place}</h5>
+        <ul>
+        <li class="card-text">${this.address}, ${this.zip} ${this.city}</li>
+        </ul>
+        </div>
       </div>
     </div>
-  </div>`
+  </div>
+`
 return showDisplay;
   }
 }
-
 
 class Restaurant extends Location1 {
   phone:string;
   webpage:string;
   foodServed:string;
-constructor(image:string, place:string, address:string, city:string, zip, phone, webpage:string, foodServed) {
-  super(image, place, address, city, zip);
+constructor(placeTitle:string, image:string, place:string, address:string, city:string, zip, phone, webpage:string, foodServed) {
+  super(placeTitle, image, place, address, city, zip);
   this.phone = phone;
   this.webpage = webpage;
   this.foodServed = foodServed;
   }
-  displayRestaurant() {
+  display() {
+  super.display();
   let showDisplayRestaurant = `
+  
   <div class="row col-lg-3 col-md-6 col-sm-12 mt-2 offset" id="restaurants">
+  <h4 class="ml-3 mt-3 mr-3 title-section"><strong>${this.placeTitle}</strong></h4>
   <div class="card">
-    <div class="card-body py-0 px-0">
-      <img src="${this.image}" alt="" height="250px" max-width="100%" class="card-img-top col-lg-12 img-responsive">
+    <div class="card-body py-0 px-0 body-section">
+      <img src="${this.image}" alt="" height="250px" max-width="100%" class="card-img-top col-lg-12 img-responsive lose-picture">
       <h5 class="card-title ml-3">${this.place}</h5>
       <ul>
       <li class="card-text">${this.address}, ${this.zip} ${this.city}</li>
@@ -60,26 +69,29 @@ return showDisplayRestaurant;
   }
 }
 
-
 class Events extends Location1 {
+
   eventDate:string;
   eventTime:string;
   price:string;
   webpage:string;
-constructor(image:string, place:string, address:string, city:string, zip, eventDate,  eventTime, price, webpage) {
-  super(image, place, address, city, zip);
+constructor(placeTitle:string, image:string, place:string, address:string, city:string, zip, eventDate,  eventTime, price, webpage) {
+  super(placeTitle, image, place, address, city, zip);
   this.eventDate = eventDate;
   this.eventTime = eventTime;
   this.price = price;
   this.webpage = webpage;
   }
-  displayEvents() {
+  display() {
+  super.display();
   let showDisplayEvents = `
+ 
   <div class="row col-lg-3 col-md-6 col-sm-12 mt-2 offset" id="events">
+   <h4 class="ml-3 mt-3 mr-3 title-section"><strong>${this.placeTitle}</strong></h4>
   <div class="card">
-    <div class="card-body py-0 px-0">
-      <img src="${this.image}" alt="" height="250px" max-width="100%" class="card-img-top col-lg-12 img-responsive">
-      <h5 class="card-title ml-3">${this.place}</h5>
+    <div class="card-body py-0 px-0 body-section">
+      <img src="${this.image}" alt="" height="250px" max-width="100%" class="card-img-top col-lg-12 img-responsive lose-picture">
+      <h5 class="card-title ml-4">${this.place}</h5>
       <ul>
       <li class="card-text">${this.address}, ${this.zip} ${this.city}</li>
       <li class="card-text">${this.eventDate}</li>
@@ -93,38 +105,24 @@ constructor(image:string, place:string, address:string, city:string, zip, eventD
 return showDisplayEvents;
   }
 }
-let karlsplatz = new Location1("img/karls.jpg", "St. Charles Church", "Karlsplatz 1", "Vienna", 1010); 
+let karlsplatz = new Location1("PLACES:", "img/karls.jpg", "St. Charles Church", "Karlsplatz 1", "Vienna", 1010); 
 
-let zoo = new Location1("img/zoo.jpg", "Zoo Vienna", "Maxingstraße 13b", "Vienna", 1130);
+let zoo = new Location1("", "img/zoo.jpg", "Zoo Vienna", "Maxingstraße 13b", "Vienna", 1130);
 
-let lemonResto = new Restaurant("img/lemon.png", "Lemon Leaf Thai Restaurant", "Kettenbrückengasse 19", "Vienna", 1050, 
-  "+43(1)5812308", "http://www.lemonleaf.at/", "Chinese Restaurant"); 
+let lemonResto = new Restaurant("RESTAURANTS:", "img/lemon.jpg", "Lemon Leaf Thai Restaurant", "Kettenbrückengasse 19", "Vienna", 1050, 
+  "+43(1)5812308", "http://www.lemonleaf.at", "Chinese Restaurant"); 
 
-let sixtaResto = new Restaurant("img/sixta.png", "Sixta Restaurant Bar", "Schönbrunner Straße 21", "Vienna", 1050, 
-  "+43 1 58 528 56", "http://www.sixta-restaurant.at/", "Viennese Restaurant");  
+let sixtaResto = new Restaurant("", "img/sixta.png", "Sixta Restaurant Bar", "Schönbrunner Straße 21", "Vienna", 1050, 
+  "+43 1 58 528 56", "http://www.sixta-restaurant.at", "Viennese Restaurant");  
 
-let krisConcert = new Events("img/kris.jpg", "Kris Kristofferson Concert", "Wiener Stadthalle(Halle E), Roland Rainer Platz 1", "Vienna", 1150, "Fr., 15.11.2019", "20:00", "58.50", "http://kriskristofferson.com/"); 
+let krisConcert = new Events("EVENTS:", "img/kris.jpg", "Kris Kristofferson Concert", "Wiener Stadthalle(Halle E), Roland Rainer Platz 1", "Vienna", 1150, "Fr., 15.11.2019", "20:00", "58.50", "http://kriskristofferson.com"); 
 
-let lennyConcert = new Events("img/lenny.jpg", "Lenny Kravitz Concert", "Wiener Stadthalle(Halle D), Roland Rainer Platz 1", "Vienna", 1150,"Sat, 09.12.2019", "19:30", "47.80", "http://www.lennykravitz.com/");
+let lennyConcert = new Events("", "img/lenny.jpg", "Lenny Kravitz Concert", "Wiener Stadthalle(Halle D), Roland Rainer Platz 1", "Vienna", 1150,"Sat, 09.12.2019", "19:30", "47.80", "http://www.lennykravitz.com");
 
 
 var array = [karlsplatz, zoo, lemonResto, sixtaResto, krisConcert, lennyConcert];
 
 var cards = document.getElementById("result");
 for (let i in array) {
-  if (i == "4") {
-  cards.innerHTML += array[i].displayEvents();
-} 
-else if (i == "5") {
-  cards.innerHTML += array[i].displayEvents();
-}
-else if (i == "2") {
-  cards.innerHTML += array[i].displayRestaurant();
-}
-else if (i == "3") {
-  cards.innerHTML += array[i].displayRestaurant();
-} 
-else {
-   cards.innerHTML += array[i].display();
-}
+  cards.innerHTML += array[i].display();
 }
